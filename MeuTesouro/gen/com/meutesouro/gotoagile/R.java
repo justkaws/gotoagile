e06f64ad97f2c244696f69dae598e3976afa9c12
George Araujo <ext-george.araujo@microsoft.com>
2014-05-07 14:09:36 -0400

    Included libs for unit testing
    Signed-off-by: George Araujo <ext-george.araujo@microsoft.com>

--- MeuTesouro/gen/com/meutesouro/gotoagile/R.java
+++ MeuTesouro/gen/com/meutesouro/gotoagile/R.java
@@ -19,32 +19,49 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int list_padding=0x7f040003;
+        public static final int shadow_width=0x7f040004;
+        public static final int slidingmenu_offset=0x7f040002;
     }
     public static final class drawable {
         public static final int favorite_checkbox=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int star_none=0x7f020002;
-        public static final int starred=0x7f020003;
+        public static final int shadow=0x7f020002;
+        public static final int shadowright=0x7f020003;
+        public static final int star_none=0x7f020004;
+        public static final int starred=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int chkIos=0x7f080003;
-        public static final int listView1=0x7f080001;
-        public static final int main_list_layout=0x7f080000;
-        public static final int money_title=0x7f080002;
+        public static final int action_settings=0x7f090007;
+        public static final int chkIos=0x7f090002;
+        public static final int listView1=0x7f090006;
+        public static final int main_frame_layout=0x7f090000;
+        public static final int menu_frame=0x7f090003;
+        public static final int money_title=0x7f090001;
+        public static final int row_icon=0x7f090004;
+        public static final int row_title=0x7f090005;
+    }
+    public static final class integer {
+        public static final int num_cols=0x7f050000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int favorite_list_item=0x7f030001;
+        public static final int list=0x7f030002;
+        public static final int menu_frame=0x7f030003;
+        public static final int row=0x7f030004;
+        public static final int title_list_layout=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_money_title_list=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int sliding_menu_starred=0x7f060005;
+        public static final int sliding_menu_titie=0x7f060004;
+        public static final int title_money_title_list=0x7f060003;
     }
     public static final class style {
         /** 
@@ -68,10 +85,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
